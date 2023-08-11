b101368c18212dc3c5a88e2e9a801bff1f295ce3
sky81219 <=>
2023-08-11 20:33:07 +0900

    sytle: 파일이관

--- core/congestion_response/congestion_data_format.py
+++ core/congestion_response/congestion_data_format.py
@@ -6,11 +6,14 @@ from pydantic import BaseModel
 from pydantic_core import ValidationError
 
 
-class AreaSexCongestionSpecific(BaseModel):
-    """여성 남성 혼잡도"""
+class TotalAgeRate(BaseModel):
+    """나이 연령별 묶음"""
 
-    male_ppltn_rate: float
-    female_ppltn_rate: float
+    area_name: str
+    area_congestion_lvl: str
+    area_congestion_msg: str
+    area_ppltn_min: float
+    area_ppltn_max: float
 
 
 class AgeCongestionSpecific(BaseModel):
@@ -26,33 +29,16 @@ class AgeCongestionSpecific(BaseModel):
     ppltn_rate_70: float
 
 
-class TotalAgeRate(BaseModel):
-    """나이 연령별 묶음"""
+class TotalAgeRateComposition(TotalAgeRate):
+    """나이대 별 혼잡도 Customizer"""
 
-    area_name: str
-    area_congestion_lvl: str
-    area_congestion_msg: str
-    area_ppltn_min: float
-    area_ppltn_max: float
-    age_congestion_specfic: dict[str, float] = AgeCongestionSpecific
+    age_congestion_specfic: AgeCongestionSpecific
 
     @classmethod
-    def _modify_arch_table(cls, ppltn_data: dict[str, str]) -> AgeCongestionSpecific:
-        """스키마 제설계"""
+    def _modify_arch_table(cls, ppltn_data: dict[str, float]) -> AgeCongestionSpecific:
         try:
-            return AgeCongestionSpecific(
-                ppltn_rate_0=ppltn_data["PPLTN_RATE_0"],
-                ppltn_rate_10=ppltn_data["PPLTN_RATE_10"],
-                ppltn_rate_20=ppltn_data["PPLTN_RATE_20"],
-                ppltn_rate_30=ppltn_data["PPLTN_RATE_30"],
-                ppltn_rate_40=ppltn_data["PPLTN_RATE_40"],
-                ppltn_rate_50=ppltn_data["PPLTN_RATE_50"],
-                ppltn_rate_60=ppltn_data["PPLTN_RATE_60"],
-                ppltn_rate_70=ppltn_data["PPLTN_RATE_70"],
-            )
+            return AgeCongestionSpecific(**ppltn_data)
         except ValidationError as error:
-            # 임시로 Exception 세부 에러는 pydantic error를 확인해서 커스텀 해야함
-            #
             logging.error("Error --> %s", error)
 
     @classmethod
@@ -63,22 +49,26 @@ class TotalAgeRate(BaseModel):
         area_congestion_msg: str,
         area_ppltn_min: float,
         area_ppltn_max: float,
-        ppltn_data: dict[str, str],
-    ) -> AgeCongestionSpecific:
-        """스키만 적용하는곳"""
-        ppltn_data: dict[str, float] = {
+        ppltn_data: dict[str, float],
+    ) -> "TotalAgeRateComposition":
+        ppltn_data = {
             key: float(value)
             for key, value in ppltn_data.items()
             if "PPLTN_RATE_" in key
         }
-        congestion_rate: AgeCongestionSpecific = cls._modify_arch_table(
-            ppltn_data=ppltn_data
-        )
+        congestion_rate = cls._modify_arch_table(ppltn_data=ppltn_data)
         return cls(
             area_name=area_name,
             area_congestion_lvl=area_congestion_lvl,
             area_congestion_msg=area_congestion_msg,
             area_ppltn_min=area_ppltn_min,
             area_ppltn_max=area_ppltn_max,
-            ppltn_data=congestion_rate,
+            age_congestion_specfic=congestion_rate,
         )
+
+
+class AreaSexCongestionSpecific(BaseModel):
+    """여성 남성 혼잡도"""
+
+    male_ppltn_rate: float
+    female_ppltn_rate: float
